6564a3b3ec194d8ccb7b329efd09c2c02da60904
Alexandru,Emilian E.M <i437101@fhict.nl>
2020-12-22 16:00:27 +0100

    Update deploy feedback footer added links to social pages

--- Website/animal-shelter/includes/main-footer.php
+++ Website/animal-shelter/includes/main-footer.php
@@ -6,16 +6,16 @@
         <section id="social-features">
             <div class="social-feature-list">
                     <div class="social-feature-image">
-                        <a id="feature-fb" href=""><i class="fab fa-facebook-f"></i></a>
+                        <a id="feature-fb" href="https://www.facebook.com"><i class="fab fa-facebook-f"></i></a>
                     </div>
                     <div class="social-feature-image sf-insta">
-                        <a id="feature-inst" href=""><i class="fab fa-instagram"></i></a>
+                        <a id="feature-inst" href="https://www.instagram.com"><i class="fab fa-instagram"></i></a>
                     </div>
                     <div class="social-feature-image">
-                        <a id="feature-twitter" href=""><i class="fab fa-twitter"></i></a>
+                        <a id="feature-twitter" href="https://twitter.com"><i class="fab fa-twitter"></i></a>
                     </div>
                     <div class="social-feature-image">
-                        <a id="feature-yt" href=""><i class="fab fa-youtube"></i></a>
+                        <a id="feature-yt" href="https://www.youtube.com"><i class="fab fa-youtube"></i></a>
                     </div>
             </div>
         </section>
